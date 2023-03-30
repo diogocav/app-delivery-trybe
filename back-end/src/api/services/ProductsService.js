cc8654bcdf71e9714fe353677c7b2c247cb3e905
Artur Senna <artursennass@gmail.com>
2023-03-30 15:44:44 -0300

    /products finalizado

--- back-end/src/api/services/ProductsService.js
+++ back-end/src/api/services/ProductsService.js
@@ -1,11 +1,11 @@
 const { Product } = require('../../database/models');
 
-const findAll = async () => {
-  const products = await Product.findOne();
+const getAll = async () => {
+  const products = await Product.findAll();
   
     return products;
   };
 
 module.exports = {
-    findAll,
+  getAll,
 };
