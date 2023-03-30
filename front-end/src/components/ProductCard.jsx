385a3d1397d0e8a351cdcf3eb543c691670a121c
Artur Senna <artursennass@gmail.com>
2023-03-30 16:20:08 -0300

    problema com datatestid de productCard

--- front-end/src/components/ProductCard.jsx
+++ front-end/src/components/ProductCard.jsx
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
 import Context from '../context/Context';
 
 export default function Products({ product }) {
@@ -6,7 +7,7 @@ export default function Products({ product }) {
   const { value, handleInputChange } = useContext(Context);
   return (
     <div key={ id }>
-      <h3 data-testid={ `customer_products__product-card-title-${id}` }>
+      <h3 data-testid={ `customer_products__element-card-title-${id}` }>
         {name}
       </h3>
       <img
@@ -15,7 +16,7 @@ export default function Products({ product }) {
         data-testid={ `customer_products__img-card-bg-image-${id}` }
         style={ { width: '200px', height: '150px' } }
       />
-      <h3 data-testid={ `customer_products__product-card-price-${id}` }>
+      <h3 data-testid={ `customer_products__element-card-price-${id}` }>
         {price}
       </h3>
       <button
@@ -41,4 +42,14 @@ export default function Products({ product }) {
     </div>
   );
 }
+
+Products.propTypes = {
+  product: PropTypes.objectOf({
+    name: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    urlImage: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
+  }).isRequired,
+};
+
 // fazer fetch dos produtos que estão em produts no provider antes fazer o endpoint no back
