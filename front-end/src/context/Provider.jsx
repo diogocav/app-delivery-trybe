1cbf93f714d6c047409fd5df228708fbb2e031ae
diogocav <diogo_c_vieira@hotmail.com>
2023-04-03 18:56:12 -0300

    fix: context

--- front-end/src/context/Provider.jsx
+++ front-end/src/context/Provider.jsx
@@ -1,123 +1,22 @@
-import React, { useState, useMemo, useEffect, useCallback } from 'react';
+import React, { useMemo, useState } from 'react';
 import PropTypes from 'prop-types';
-import { useHistory } from 'react-router-dom';
-import jwtDecode from 'jwt-decode';
 import Context from './Context';
-import fetchApi from '../services/fetchApi';
 
 function Provider({ children }) {
-  const [isDisabled, setIsDisabled] = useState(true);
-  const [isDisabledR, setIsDisabledR] = useState(true);
-  const [isDisabledLoginError, setIsDisabledLoginError] = useState(false);
-  const [isDisabledRegisterError, setIsDisabledRegisterError] = useState(false);
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [name, setName] = useState('');
   const [updatedValueProducts, setUpdatedValueProducts] = useState(false);
-  // const [value, setValue] = useState(0);
-  const history = useHistory();
-
-  useEffect(() => {
-    const verifyBtn = () => {
-      const validEmail = /\S+@\S+\.\S+/;
-      const verifyEmail = validEmail.test(email);
-      const numeroMin = 6;
-      const numeroName = 12;
-      const verifyName = name.length >= numeroName;
-      const verifyPassword = password.length >= numeroMin;
-      if ((verifyName && verifyEmail && verifyPassword)) {
-        return setIsDisabledR(false);
-      }
-      if ((verifyPassword && verifyEmail)) {
-        setIsDisabledR(true);
-        return setIsDisabled(false);
-      }
-      setIsDisabled(true);
-      return setIsDisabledR(true);
-    };
-    verifyBtn();
-  }, [email, password, name]);
-
-  const handleEmail = useCallback(({ target }) => {
-    setEmail(target.value);
-  }, [setEmail]);
-
-  const handlePassword = useCallback(({ target }) => {
-    setPassword(target.value);
-  }, [setPassword]);
-
-  const handleName = useCallback(({ target }) => {
-    setName(target.value);
-  }, [setName]);
-
-  // const handleInputChange = useCallback((newValue) => {
-  //   setValue((oldValue) => oldValue + newValue);
-  // }, [setValue]);
-
-  const handleClickLogin = useCallback(async () => {
-    const result = await fetchApi('POST', 'login', { email, password });
-    if (result.message !== undefined) setIsDisabledLoginError(true);
-    if (result.token) {
-      const { token } = result;
-      const decoded = jwtDecode(token);
-      history.push('/customer/products');
-      localStorage.setItem('user', JSON.stringify({
-        ...decoded.data,
-        token }));
-    }
-    // localStorage.setItem('user', JSON.stringify({ email }));
-  }, [setIsDisabledLoginError, email, password, history]);
-
-  const handleClickRegister = useCallback(async () => {
-    const result = await fetchApi('POST', 'register', { name, email, password });
-    if (result.message === 'account created') {
-      history.push('/customer/products');
-    } else {
-      setIsDisabledRegisterError(true);
-    }
-  }, [setIsDisabledRegisterError, name, email, password, history]);
 
   const context = useMemo(() => ({
-    email,
-    password,
-    name,
-    isDisabled,
-    isDisabledR,
-    isDisabledLoginError,
-    isDisabledRegisterError,
-    handleName,
-    handleEmail,
-    handlePassword,
-    handleClickLogin,
-    handleClickRegister,
-    // handleInputChange,
     updatedValueProducts,
     setUpdatedValueProducts,
   }), [
-    name,
-    email,
-    password,
-    isDisabled,
-    isDisabledR,
-    isDisabledLoginError,
-    isDisabledRegisterError,
-    handleName,
-    handleEmail,
-    handlePassword,
-    handleClickLogin,
-    handleClickRegister,
-    // handleInputChange,
     updatedValueProducts,
-    setUpdatedValueProducts,
-  ]);
-
+    setUpdatedValueProducts]);
   return (
     <Context.Provider value={ context }>
       { children }
     </Context.Provider>
   );
 }
-
 Provider.propTypes = {
   children: PropTypes.node.isRequired,
 };
--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -1,17 +1,35 @@
-import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
-import Context from '../context/Context';
+import React, { useState, useCallback } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import jwtDecode from 'jwt-decode';
+import fetchApi from '../services/fetchApi';
 
 export default function Login() {
-  const {
-    email,
-    password,
-    handleEmail,
-    handlePassword,
-    handleClickLogin,
-    isDisabled,
-    isDisabledLoginError,
-  } = useContext(Context);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isDisabledLoginError, setIsDisabledLoginError] = useState(false);
+  const history = useHistory();
+  const handleChange = (value, func) => func(value);
+  const validateEmail = (emailInput) => {
+    const validEmail = /\S+@\S+\.\S+/;
+    return validEmail.test(emailInput);
+  };
+  const validatePassword = (passwordInput) => {
+    const minPasswordLength = 6;
+    return passwordInput.length >= minPasswordLength;
+  };
+  const validateLogin = () => validateEmail(email) && validatePassword(password);
+  const handleClickLogin = useCallback(async () => {
+    const result = await fetchApi('POST', 'login', { email, password });
+    if (result.message !== undefined) setIsDisabledLoginError(true);
+    if (result.token) {
+      const { token } = result;
+      const decoded = jwtDecode(token);
+      history.push('/customer/products');
+      localStorage.setItem('user', JSON.stringify({
+        ...decoded.data,
+        token }));
+    }
+  }, [setIsDisabledLoginError, email, password, history]);
   return (
     <div className="login">
       <form>
@@ -20,23 +38,21 @@ export default function Login() {
           data-testid="common_login__input-email"
           placeholder="E-mail"
           value={ email }
-          onChange={ handleEmail }
+          onChange={ ({ target: { value } }) => (handleChange(value, setEmail)) }
         />
-
         <input
           className="password"
           type="password"
           data-testid="common_login__input-password"
           placeholder="Password"
           value={ password }
-          onChange={ handlePassword }
+          onChange={ ({ target: { value } }) => (handleChange(value, setPassword)) }
         />
-
         <button
           className="button"
           data-testid="common_login__button-login"
           type="button"
-          disabled={ isDisabled }
+          disabled={ !validateLogin() }
           onClick={ handleClickLogin }
         >
           login
