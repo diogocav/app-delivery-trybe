241b9d83fad82df5f6a52c0eaf5a6e4c737b6107
Artur Senna <artursennass@gmail.com>
2023-04-11 17:41:03 -0300

    testes de integração do login feitos

--- back-end/src/tests/integration/login.test.js
+++ back-end/src/tests/integration/login.test.js
@@ -2,10 +2,10 @@ const sinon = require('sinon');
 const { expect } = require('chai');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+// const crypto = require('crypto');
 const app = require('../../api/app');
 const generateToken = require('../../api/middlewares/generateToken');
 const { User } = require('../../database/models');
-const crypto = require('crypto');
 
 chai.use(chaiHttp);
 
@@ -13,7 +13,7 @@ describe('Teste para a rota /login', function () {
   const LOGIN_INPUT = {
     email: 'zebirita@email.com',
     password: '$#zebirita#$',
-  }
+  };
 
   const LOGIN_OUTPUT = {
       id: '3',
@@ -23,8 +23,8 @@ describe('Teste para a rota /login', function () {
       role: 'customer',
     };
 
-  const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjoiMyIsIm5hbWUiOiJDbGllbnRlIFrDqSBCaXJpdGEiLCJlbWFpbCI6InplYmlyaXRhQGVtYWlsLmNvbSIsInJvbGUiOiJjdXN0b21lciJ9LCJpYXQiOjE2ODA3MjMwNDUsImV4cCI6MTY4MTMyNzg0NX0.5BKHgTmPgSai5CDNS9Dxz2t4ua1fPDhc9eZJyl73SKA'
-
+  const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjoiMyIsIm5hbWUiOiJDbGllbnRlIFrDqSBCaXJpdGEiLCJlbWFpbCI6InplYmlyaXRhQGVtYWlsLmNvbSIsInJvbGUiOiJjdXN0b21lciJ9LCJpYXQiOjE2ODA3MjMwNDUsImV4cCI6MTY4MTMyNzg0NX0.5BKHgTmPgSai5CDNS9Dxz2t4ua1fPDhc9eZJyl73SKA';
+  
   beforeEach(function () {
     sinon.stub(User, 'findOne');
   });
@@ -34,7 +34,7 @@ describe('Teste para a rota /login', function () {
   });
   
   it('1- Deve fazer um login com sucesso', async function () {
-    sinon.stub(generateToken, 'GenerateNewToken').resolves(TOKEN);
+    sinon.stub(generateToken, 'GenerateNewToken').returns(TOKEN);
     User.findOne.resolves(LOGIN_OUTPUT);
 
     // Action
@@ -46,17 +46,14 @@ describe('Teste para a rota /login', function () {
 // console.log(chaiHttpResponse);
     // Assertion
     expect(chaiHttpResponse.status).to.be.equals(200);
-    expect(chaiHttpResponse.body).to.equal({token: TOKEN});
-
-    sinon.restore();
+    expect(chaiHttpResponse.body).to.be.deep.equal({ token: TOKEN });
   });
 
   it('2- Deve retornar erro com email incorreto', async function () {
     const loginInputEmail = {
         email: 'zeb@email.com',
         password: '$#zebirita#$',
-      }
-    
+      };
 
     User.findOne.resolves(null);
     // sinon.stub(User, 'findOne').resolves(null);
@@ -69,17 +66,14 @@ describe('Teste para a rota /login', function () {
 
     // Assertion
     expect(chaiHttpResponse.status).to.be.equals(404);
-    expect(chaiHttpResponse.body).to.equal({message: 'Invalid email'});
-
-    sinon.restore();
+    expect(chaiHttpResponse.body).to.be.deep.equal({ message: 'Invalid email' });
   });
 
   it('3- Deve retornar erro com senha incorreta', async function () {
     const loginInputPassword = {
         email: 'zebirita@email.com',
         password: '$#zebirit',
-      }
-    
+      };
 
     User.findOne.resolves(LOGIN_OUTPUT);
     // sinon.stub(User, 'findOne').resolves(LOGIN_OUTPUT);
@@ -92,16 +86,14 @@ describe('Teste para a rota /login', function () {
 
     // Assertion
     expect(chaiHttpResponse.status).to.be.equals(404);
-    expect(chaiHttpResponse.body).to.equal({message: 'Invalid password'});
-
-    sinon.restore();
+    expect(chaiHttpResponse.body).to.be.deep.equal({ message: 'Invalid password' });
   });
 
   it('4- Deve retornar erro com email inválido', async function () {
     const loginInputPassword = {
         email: 'zebiritaemail.com',
         password: '$#zebirit',
-      }
+      };
 
     // Action
     const chaiHttpResponse = await chai
@@ -111,16 +103,14 @@ describe('Teste para a rota /login', function () {
 
     // Assertion
     expect(chaiHttpResponse.status).to.be.equals(404);
-    expect(chaiHttpResponse.body).to.contains({message: 'Invalid email or password'});
-
-    sinon.restore();
+    expect(chaiHttpResponse.body).to.contains({ message: 'Invalid email or password' });
   });
 
   it('5- Deve retornar erro com senha inválida', async function () {
     const loginInputPassword = {
         email: 'zebirita@email.com',
         password: '$#z',
-      }
+      };
 
     // Action
     const chaiHttpResponse = await chai
@@ -130,16 +120,14 @@ describe('Teste para a rota /login', function () {
 
     // Assertion
     expect(chaiHttpResponse.status).to.be.equals(404);
-    expect(chaiHttpResponse.body).to.contains({message: 'Invalid email or password'});
-
-    sinon.restore();
+    expect(chaiHttpResponse.body).to.contains({ message: 'Invalid email or password' });
   });
 
   it('6- Deve retornar erro com email e senha vazios', async function () {
     const loginInputPassword = {
         email: '',
         password: '',
-      }
+      };
 
     // Action
     const chaiHttpResponse = await chai
@@ -149,9 +137,6 @@ describe('Teste para a rota /login', function () {
 
     // Assertion
     expect(chaiHttpResponse.status).to.be.equals(404);
-    expect(chaiHttpResponse.body).to.contains({message: 'All fields must be filled'});
-
-    sinon.restore();
+    expect(chaiHttpResponse.body).to.contains({ message: 'All fields must be filled' });
   });
-
 });
